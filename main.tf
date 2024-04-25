d9b54238b32df03c7067eb8d276aa9d17ba32e00
Chrysovaladis Zaloumis <chrysozalo@gmail.com>
2024-04-25 18:41:28 +0300

    Add Load Balancer Controller to be able to use ingress for k8s.

--- main.tf
+++ main.tf
@@ -54,7 +54,20 @@ module "eks" {
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
+  control_plane_subnet_ids       = module.vpc.private_subnets
   cluster_endpoint_public_access = true
+
+  cluster_addons = {
+     coredns = {
+     most_recent = true
+     }
+     kube-proxy = {
+     most_recent = true
+     }
+     vpc-cni = {
+     most_recent = true
+     }
+  }
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
@@ -109,3 +122,178 @@ resource "aws_eks_addon" "ebs-csi" {
     "terraform" = "true"
   }
 }
+
+# data "tls_certificate" "eks" {
+#   # url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+#   url = module.eks.cluster_oidc_issuer_url
+# }
+
+# resource "aws_iam_openid_connect_provider" "eks" {
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+#   url             = module.eks.cluster_oidc_issuer_url # aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+# }
+
+data "aws_iam_policy_document" "aws_load_balancer_controller_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks.oidc_provider, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+
+    principals {
+      identifiers = [module.eks.oidc_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  assume_role_policy = data.aws_iam_policy_document.aws_load_balancer_controller_assume_role_policy.json
+  name               = "aws-load-balancer-controller"
+}
+
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  policy = file("./AWSLoadBalancerController.json")
+  name   = "AWSLoadBalancerController"
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+}
+
+output "aws_load_balancer_controller_role_arn" {
+  value = aws_iam_role.aws_load_balancer_controller.arn
+}
+
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data) # aws_eks_cluster.cluster.certificate_authority[0].data
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name] # aws_eks_cluster.cluster.id]
+      command     = "aws"
+    }
+  }
+}
+
+resource "helm_release" "aws-load-balancer-controller" {
+  name = "aws-load-balancer-controller"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.4.1"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v2.4.2"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.aws_load_balancer_controller.arn
+  }
+
+  depends_on = [
+    module.eks.eks_managed_node_groups, # aws_eks_node_group.private-nodes,
+    aws_iam_role_policy_attachment.aws_load_balancer_controller_attach
+  ]
+}
+
+# module "lb_role" {
+#  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+#  role_name                              = "${var.env_name}_eks_lb"
+#  attach_load_balancer_controller_policy = true
+
+#  oidc_providers = {
+#      main = {
+#      provider_arn               = module.eks.oidc_provider_arn # var.oidc_provider_arn
+#      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+#      }
+#   }
+#  }
+
+#  provider "kubernetes" {
+#   config_path    = "~/.kube/config"
+# }
+
+
+#  resource "kubernetes_service_account" "service-account" {
+#   metadata {
+#       name      = "aws-load-balancer-controller"
+#       namespace = "kube-system"
+#       labels = {
+#       "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+#       "app.kubernetes.io/component" = "controller"
+#       }
+#       annotations = {
+#       "eks.amazonaws.com/role-arn"               = module.lb_role.iam_role_arn
+#       "eks.amazonaws.com/sts-regional-endpoints" = "true"
+#       }
+#   }
+#  }
+
+#  provider "helm" {
+#   kubernetes {
+#     config_path = "~/.kube/config"
+#   }
+#  }
+
+#  resource "helm_release" "alb-controller" {
+#  name       = "aws-load-balancer-controller"
+#  repository = "https://aws.github.io/eks-charts"
+#  chart      = "aws-load-balancer-controller"
+#  namespace  = "kube-system"
+#  depends_on = [
+#      kubernetes_service_account.service-account
+#  ]
+
+#  set {
+#      name  = "region"
+#      value = var.region
+#  }
+
+#  set {
+#      name  = "vpcId"
+#      value = module.vpc.vpc_id
+#  }
+
+#  set {
+#      name  = "image.repository"
+#      value = "602401143452.dkr.ecr.${var.region}.amazonaws.com/amazon/aws-load-balancer-controller"
+#  }
+
+#  set {
+#      name  = "serviceAccount.create"
+#      value = "false"
+#  }
+
+#  set {
+#      name  = "serviceAccount.name"
+#      value = "aws-load-balancer-controller"
+#  }
+
+#  set {
+#      name  = "clusterName"
+#      value = module.eks.cluster_name
+#  }
+#  }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type        = string
   default     = "eu-central-1"
 }
+
+variable "env_name" {
+  description = "Enviroment Name"
+  type        = string
+  default     = "dev"
+}
